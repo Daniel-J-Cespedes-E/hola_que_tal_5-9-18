aa158fe742a98f69e730f84e93a26d2482c47c2a
Daniel-J-Cespedes-E <danieljce12@gmail.com>
2018-09-05 08:15:19 -0300

    que haras?

---
+++ hola.py
@@ -0,0 +1,9 @@
+print"help me"
+while(True):
+	p = input("me ayudas?(si/no): ")
+	if(p == "si"):
+		print("gracias buen samaritano")
+		break
+	elif(p == "no"):
+		print("gracias, chao")
+		break
---
+++ holaMundo2.py
@@ -0,0 +1,13 @@
+
+#Autor: Yon White
+#Fecha 5-9-18
+#Codigo de prueba para utilizacion de git y github
+
+print ("hello my fuking world")
+print ("XD")
+
+while(True):
+        p = inst("Que tal")
+        if(p == "que te den")
+                break
+
